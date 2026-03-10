e4faa1638c5a201ac992a002b11032d67d2cc4fa
Charles Kagiri <charleskagiri@gmail.com>
2026-03-10 13:56:18 +0300

    feat: redirect / to my-table for authenticated users

--- api/src/main/java/com/ligitabl/api/web/PublicController.java
+++ api/src/main/java/com/ligitabl/api/web/PublicController.java
@@ -1,5 +1,6 @@
 package com.ligitabl.api.web;
 
+import java.security.Principal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +11,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PublicController {
     @GetMapping("/")
-    public String home(Model model) {
+    public String home(Model model, Principal principal) {
+        if (principal != null) {
+            return "redirect:/my-table";
+        }
+        model.addAttribute("pageTitle", "Home");
+        return "index";
+    }
+
+    @GetMapping("/home")
+    public String homeNoRedirect(Model model) {
         model.addAttribute("pageTitle", "Home");
         return "index";
     }
