f925947f358bae98c98ab19340cff0ecf9a54bc2
Charles Kagiri <charleskagiri@gmail.com>
2026-07-03 16:20:32 +0300

    fix: resolve tests failures caused by missing fk_season_id following recent t_match migration

--- api/src/test/java/com/ligitabl/api/rest/match/MatchRepositoryIntegrationTest.java
+++ api/src/test/java/com/ligitabl/api/rest/match/MatchRepositoryIntegrationTest.java
@@ -116,11 +116,12 @@ class MatchRepositoryIntegrationTest extends AbstractPostgresIT {
         String slug = "home-vs-away";
 
         jdbcTemplate.update(
-                "INSERT INTO t_match (pk_id, c_client_id, fk_round_id, fk_home_team_id, fk_away_team_id, c_slug, c_status, c_kick_off, c_venue, c_matchday) "
-                        + "VALUES (?,?,?,?,?,?,?,?,?,?)",
+                "INSERT INTO t_match (pk_id, c_client_id, fk_round_id, fk_season_id, fk_home_team_id, fk_away_team_id, c_slug, c_status, c_kick_off, c_venue, c_matchday) "
+                        + "VALUES (?,?,?,?,?,?,?,?,?,?,?)",
                 matchId,
                 1,
                 roundId,
+                seasonId,
                 homeTeamId,
                 awayTeamId,
                 slug,
